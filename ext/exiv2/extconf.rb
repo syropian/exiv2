26d8878e3b803608e34f96f976052a53ed52120c
syropian <collinpatrickhenderson@gmail.com>
2019-05-27 01:55:36 -0400

    Revert to master version of extconf file

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -1,8 +1,10 @@
 require 'mkmf'
-config = pkg_config('exiv2', '')
-if config
-  $CPPFLAGS = [$CPPFLAGS, config].join ' '
-  create_makefile("exiv2/exiv2")
-else
-  abort 'exiv2 devel package is missing'
-end
+
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+RbConfig::MAKEFILE_CONFIG['CCFLAGS'] = ENV['CCFLAGS'] if ENV['CCFLAGS']
+RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
+RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] = ENV['CXXFLAGS'] if ENV['CXXFLAGS']
+
+dir_config("exiv2")
+have_library("exiv2")
+create_makefile("exiv2/exiv2")
