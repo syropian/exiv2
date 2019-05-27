cc3ade71a18b0534722cdfcf96e884e03ca861c6
syropian <collinpatrickhenderson@gmail.com>
2019-05-27 01:36:55 -0400

    Explicitly passes cflags option to pkg_config call

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
-config = pkg_config('exiv2')
+config = pkg_config('exiv2', 'cflags')
 if config
-  $CPPFLAGS = [$CPPFLAGS, config[0]].join ' '
+  $CPPFLAGS = [$CPPFLAGS, config].join ' '
   create_makefile("exiv2/exiv2")
 else
   abort 'exiv2 devel package is missing'
